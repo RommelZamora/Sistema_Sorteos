02a2ddae013736c6e95ba98264a760f171c78ed0
JeanVillamar <jfvillam@espol.edu.ec>
2024-05-28 00:21:13 -0500

    Se agrego las fotos y los datos de las fotos en Facebook, en instagram aún falta al autorización

--- passportConfig.js
+++ passportConfig.js
@@ -36,31 +36,32 @@ async (req, accessToken, refreshToken, profile, done) => {
 }));
 
 passport.use(new FacebookStrategy({
-    clientID: '1171945480666514', // Reemplaza con tu clientID de Facebook
-    clientSecret: 'f800630abadf71f993634edf0620335b', // Reemplaza con tu clientSecret de Facebook
-    callbackURL: 'https://localhost:3200/auth/facebook/callback'
+    clientID: '2616988788481233', // Reemplaza con tu clientID de Facebook
+    clientSecret: '269cd70fdbf815535e7ff7b52456f2a7', // Reemplaza con tu clientSecret de Facebook
+    callbackURL: 'https://localhost:3200/auth/facebook/callback',
+    scope: ['email', 'user_photos'], // Asegúrate de solicitar los permisos adecuados
+    passReqToCallback: true
 },
-async (accessToken, refreshToken, profile, done) => {
+async (req, accessToken, refreshToken, profile, done) => {
     try {
-        console.log('Access Token:', accessToken);
-        console.log('Profile:', profile);
+        // Obtener el perfil del usuario con el token de acceso
+        const userResponse = await axios.get(`https://graph.facebook.com/me?fields=id,photos{created_time,id,name,link,images}&access_token=${accessToken}`);
+        const user = userResponse.data;
 
-        // Devuelve el perfil del usuario directamente
-        return done(null, profile);
+        return done(null, user); // Devuelve el usuario directamente
     } catch (err) {
         console.error('Error fetching user profile:', err);
+        console.error('Response data:', err.response ? err.response.data : 'No response data');
         return done(err);
     }
 }));
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user);
 });
 
-passport.deserializeUser((id, done) => {
-    // Dado que no estamos almacenando los usuarios en la base de datos,
-    // simplemente devuelve un objeto de usuario con el id
-    done(null, { id });
+passport.deserializeUser((user, done) => {
+    done(null, user);
 });
 
 module.exports = passport;
